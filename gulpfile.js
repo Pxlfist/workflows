87330094c3419f396807aaccebdafca1b6819901
Pxlfist <pxlfist@gmail.com>
2016-05-13 10:12:25 -0600

    added CoffeeScript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,12 @@
-// This file tells Gulp what to do when it runs
-
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	coffee = require('gulp-coffee');
 
-// create a task using the task method
+var coffeeSources = ['components/coffee/tagline.coffee'];
 
 gulp.task('coffee', function() {
-	gulp.src('components/coffee/tagline.coffee')
-		// Doesn't put the out-going JavaScript in a safety wrapper
-		.pipe(coffee({ bare: true})
+	gulp.src(coffeeSources)
+		.pipe(coffee({ bare: true })
 			.on('error', gutil.log))
-
-		// New method where we're sending the file to once it's done
 		.pipe(gulp.dest('components/scripts'))
-
 });

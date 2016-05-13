9809c6aa86c6673c299478ba8ed21b9c4a14a577
Pxlfist <pxlfist@gmail.com>
2016-05-13 09:55:59 -0600

    Gulpfiled

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
 // This file tells Gulp what to do when it runs
 
 var gulp = require('gulp'),
-	gutil = require('gulp-util');
+	gutil = require('gulp-util'),
+	coffee = require('gulp-coffee');
 
 // create a task using the task method
 
-gulp.task('log', function(){
+gulp.task('coffee', function() {
+	gulp.src('components/coffee/tagline.coffee')
+		// Doesn't put the out-going JavaScript in a safety wrapper
+		.pipe(coffee({ bare: true})
+			.on('error', gutil.log))
 
-	gutil.log('Workflows are awesome');
+		// New method where we're sending the file to once it's done
+		.pipe(gulp.dest('components/scripts'))
 
 });
